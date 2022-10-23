696f1b32d2398d60ec21021c76ac9a28b6aa78ae
javierhuebra <javieremanuelhuebra@gmail.com>
2022-10-22 23:02:51 -0300

    feat: testing a way to type console.log easy

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import './App.css';
 
 function App() {
+  const c = console.log.bind(document);
+  c("que onda esta");
+  
   return (
     <div className="App">
-      
+      Esta empezando el ecomerce!
     </div>
   );
 }
